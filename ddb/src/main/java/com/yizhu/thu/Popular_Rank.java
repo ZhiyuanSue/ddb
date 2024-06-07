303925ef7825560dcdc81481c2d5147cf34830eb
ZhiyuanSue <2262387848@qq.com>
2024-06-07 11:28:53 +0800

    update code

--- ddb/src/main/java/com/yizhu/thu/Popular_Rank.java
+++ ddb/src/main/java/com/yizhu/thu/Popular_Rank.java
@@ -19,6 +19,10 @@ import java.util.Date;
 import java.time.DayOfWeek;  
 import java.time.LocalDate;  
 import java.time.temporal.TemporalAdjusters;  
+import java.util.PriorityQueue;  
+import java.util.Collections;
+import java.util.stream.Collectors;  
+import java.util.Arrays;  
 
 public class Popular_Rank extends Table {
 	static String table_name = "popular_rank";
@@ -42,6 +46,7 @@ public class Popular_Rank extends Table {
 		insert_new_table(conn_user_2,table_name,table_sql);
 		insert_new_table(conn_user_3,table_name,table_sql);
 		bulk(2);
+		bulk(3);
 		close(2);
 		close(3);
 	}
@@ -52,7 +57,9 @@ public class Popular_Rank extends Table {
 				+"readNum "
 				+"FROM be_read";
 		
-		Map<Integer,List<String>> timestamp_aid=new HashMap<>();
+		Map<Long,List<String>> timestamp_aid_daily=new HashMap<>();
+		Map<Long,List<String>> timestamp_aid_weekly=new HashMap<>();
+		Map<Long,List<String>> timestamp_aid_monthly=new HashMap<>();
 		Map<String,Integer> aid_readnum=new HashMap<>();
 		//read the be_read table
 		try (Statement stmt = conn_user_2.createStatement();  
@@ -60,28 +67,123 @@ public class Popular_Rank extends Table {
   
             while (rs.next()) {
 				String aid = rs.getString("aid");
-				String readNum = rs.getString("readNum");
+				int readNum = rs.getInt("readNum");
 				String timestamp = rs.getString("timestamp");
 				long timestampMillis = Long.parseLong(timestamp);  
-				Instant instantMillis = Instant.ofEpochMilli(timestampMillis);  
-				LocalDateTime dateTimeMillis = instantMillis.atZone(ZoneId.systemDefault()).toLocalDateTime();  
-				
+
+				long timestamp_daily_start = getDayStartTimestamp(timestampMillis);
+				long timestamp_weekly_start = getWeekStartTimestamp(timestampMillis);
+				long timestamp_monthly_start = getMonthStartTimestamp(timestampMillis);
+				// daily
+				if (!timestamp_aid_daily.containsKey(timestamp_daily_start)){
+					List<String> aidlist=new ArrayList<>();
+					timestamp_aid_daily.put(timestamp_daily_start,aidlist);
+				}
+				timestamp_aid_daily.get(timestamp_daily_start).add(aid);
+				// weekly
+				if (!timestamp_aid_weekly.containsKey(timestamp_weekly_start)){
+					List<String> aidlist=new ArrayList<>();
+					timestamp_aid_weekly.put(timestamp_weekly_start,aidlist);
+				}
+				timestamp_aid_weekly.get(timestamp_weekly_start).add(aid);
+				//monthly
+				if (!timestamp_aid_monthly.containsKey(timestamp_monthly_start)){
+					List<String> aidlist=new ArrayList<>();
+					timestamp_aid_monthly.put(timestamp_monthly_start,aidlist);
+				}
+				timestamp_aid_monthly.get(timestamp_monthly_start).add(aid);
+
+				aid_readnum.put(aid,readNum);
 			}
+
 		}catch (SQLException e) {  
             e.printStackTrace();  
         } 
 		
+		if(dbms_num == 2){	// dbms1, hadoop2, store the daily data
+			String temporalGranularity = "daily";
+			int id=0;
+			for(long ts: timestamp_aid_daily.keySet()){
+				List<String> aids= timestamp_aid_daily.get(ts);
+
+				List<String> maxfive = findFiveLargest(aids,aid_readnum);
+
+				String aidlist="";
+				for (String aid:maxfive){
+					aidlist+=aid+",";
+				}
+				StringBuilder sb = new StringBuilder();  
+				sb.append(id);  
+				String id_str=sb.toString();
+				insert(
+					conn_user_2,
+					id_str,
+					String.valueOf(ts),
+					temporalGranularity,
+					aidlist
+				);
+				id++;
+			}
+		} else if(dbms_num==3){	// dbms2, hadoop3, store the weekly and monthly data
+			String temporalGranularity1 = "weekly";
+			String temporalGranularity2 = "monthly";
+			int id=0;
+			for(long ts: timestamp_aid_weekly.keySet()){
+				List<String> aids= timestamp_aid_weekly.get(ts);
+				
+				List<String> maxfive = findFiveLargest(aids,aid_readnum);
+
+				String aidlist="";
+				for (String aid:maxfive){
+					aidlist+=aid+",";
+				}
+				StringBuilder sb = new StringBuilder();  
+				sb.append(id);  
+				String id_str=sb.toString();
+				insert(
+					conn_user_3,
+					id_str,
+					String.valueOf(ts),
+					temporalGranularity1,
+					aidlist
+				);
+				id++;
+			}
+			for(long ts: timestamp_aid_monthly.keySet()){
+				List<String> aids= timestamp_aid_monthly.get(ts);
+				
+				List<String> maxfive = findFiveLargest(aids,aid_readnum);
+
+				String aidlist="";
+				for (String aid:maxfive){
+					aidlist+=aid+",";
+				}
+				StringBuilder sb = new StringBuilder();  
+				sb.append(id);  
+				String id_str=sb.toString();
+				insert(
+					conn_user_3,
+					id_str,
+					String.valueOf(ts),
+					temporalGranularity2,
+					aidlist
+				);
+				id++;
+			}
+		}
+		
 	}
 	public void insert(
 		Connection conn,
-		Timestamp timestamp,
+		String id,
+		String timestamp,
 		String temporalGranularity,
 		String articleAidList
 	){
 		
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {  
-		
-			pstmt.setTimestamp(2, timestamp);  
+			pstmt.setString(1, id);
+			pstmt.setString(2, timestamp);  
 			pstmt.setString(3, temporalGranularity);  
 			pstmt.setString(4, articleAidList);  
 		
@@ -158,4 +260,30 @@ public class Popular_Rank extends Table {
 		LocalDate monthStart = localDate.withDayOfMonth(1);  
 		return monthStart.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();  
 	}  
+
+	public static List<String> findFiveLargest(List<String> aidlist,Map<String,Integer> aid_readnum) {  
+		String[] aidarray = new String[5]; 
+		for (int i=0;i<5;i++){
+			aidarray[i]="";
+		}
+
+        for (String aid : aidlist) {  
+			for(int i=0;i<5;i++){
+				if(aidarray[i]==""){
+					aidarray[i]=aid;
+					break;
+				}
+				else{
+					int cur_num=aid_readnum.get(aid);
+					int tmp_array_num=aid_readnum.get(aidarray[i]);
+					if(cur_num>tmp_array_num){
+						aidarray[i]=aid;
+						break;
+					}
+				}
+			}
+        }  
+  
+        return Arrays.asList(aidarray);
+    }  
 }
